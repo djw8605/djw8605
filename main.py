4d44450f34b3c246a378d3411d4647ef725a5906
Derek Weitzel <djw8605@gmail.com>
2021-10-01 11:02:06 -0500

    Adding initial auto-updateing README

---
+++ main.py
@@ -0,0 +1,23 @@
+import jinja2
+import feedparser
+
+def getBlogPosts():
+    d = feedparser.parse('https://derekweitzel.com/feed.xml')
+    return d.entries[:5]
+
+def main():
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "README.md.tmpl"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    posts = getBlogPosts()
+
+    outputText = template.render(posts=posts)
+    with open("README.md", 'w') as readme:
+        readme.write(outputText)
+    
+
+
+if __name__ == "__main__":
+    main()
